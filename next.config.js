1ff4b56a66422eab71d74513926d3e6814c0a038
Juliano <julianofirme23@gmail.com>
2023-06-07 01:40:47 -0300

    chore: next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+}
 
 module.exports = nextConfig
